f92ff9f0133189eed3aa8aacbca31a643b3873a3
Stéphane Maniaci <stephane.maniaci@gmail.com>
2016-09-12 17:26:43 +0100

    [emacs] ido -> ivy/swiper/avy

--- .emacs.init.el
+++ .emacs.init.el
@@ -15,6 +15,26 @@
                     :width 'normal
                     :height 160)
 
+;; swiper
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "<f6>") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f1> l") 'counsel-load-library)
+(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+(global-set-key (kbd "C-c g") 'counsel-git)
+(global-set-key (kbd "C-c j") 'counsel-git-grep)
+(global-set-key (kbd "C-c k") 'counsel-ag)
+(global-set-key (kbd "C-x l") 'counsel-locate)
+(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+(define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
+
 ;; mac keys
 (setq mac-command-modifier 'meta
       mac-option-modifier 'super)
@@ -24,12 +44,6 @@
 (setq tab-width 2)
 (setq-default tab-width 2)
 (setq indent-line-function 'insert-tab)
-
-;; ido
-(setq
- ido-everywhere t
- ido-vertical-mode t)
-(ido-mode t)
 
 ;; js2-mode
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
