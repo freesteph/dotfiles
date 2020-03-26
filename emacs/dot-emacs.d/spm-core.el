34f9a52c013dffc0d143873094efd7daee7ed9f3
Stéphane Maniaci <stephane.maniaci@gmail.com>
2020-03-26 13:36:57 +0000

    emacs: add a ruby macro

--- emacs/dot-emacs.d/spm-core.el
+++ emacs/dot-emacs.d/spm-core.el
@@ -80,6 +80,14 @@
                                               (if (match-string 2) "" ", focus: true ")
                                               (match-string 3)))))))))
 
+(defun spm/ruby/insert-frozen-string-literal ()
+  "Insert the frozen string literal on the first buffer line."
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (insert "# frozen_string_literal: true")
+    (open-line 2)))
+
 (add-hook 'ruby-mode-hook
           (lambda ()
             (local-set-key (kbd "C-c C-f") 'spm/ruby/toggle-rspec-focus)))
