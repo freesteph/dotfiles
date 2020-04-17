0a20137dc3e790fa6c924f780c64de03b69eaeeb
Stéphane Maniaci <stephane.maniaci@gmail.com>
2020-04-17 13:04:58 +0100

    emacs: remove some CSS hackery

--- emacs/.emacs.d/spm/spm-css.el
+++ emacs/.emacs.d/spm/spm-css.el
@@ -1,23 +1,11 @@
-(defun spm/css/sort-properties ()
-  "Sort all the properties in a CSS block alphabetically."
-  (interactive)
-  (save-excursion
-    (backward-up-list)
-    (unless (not (eql (char-after (point)) ?{))
-      (let ((beg (point)))
-        (forward-list)
-        (forward-line -1)
-        (end-of-line)
-        (let ((end (point)))
-          (goto-char beg)
-          (forward-line)
-          (beginning-of-line)
-          (sort-lines nil (point) end))))))
-
 (defun spm/css-mode-hook ()
   "Tack some keybindings over `css-mode'."
+  (add-to-list
+   'load-path (concat
+               (getenv "DEV_ROOT")
+               "/steph/gh/css-sort/"))
   (define-key css-mode-map
-    (kbd "C-c C-s") 'spm/css/sort-properties))
+    (kbd "C-c C-s") 'css-sort))
 
 (add-hook 'css-mode-hook 'spm/css-mode-hook)
 
