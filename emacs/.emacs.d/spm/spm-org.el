852730f14e4af603bad51de986197623bc727020
Stéphane Maniaci <stephane.maniaci@gmail.com>
2020-08-11 12:36:14 +0100

    emacs: tweaks for org

--- emacs/.emacs.d/spm/spm-org.el
+++ emacs/.emacs.d/spm/spm-org.el
@@ -1,3 +1,14 @@
+;;; spm-org -- org-related things
+
+;;; Commentary:
+;;; Customisation for org.
+
+;;; Code:
 (setf org-todo-keywords
       '((sequence "TODO" "DONE")
-        (sequence "TODO" "DISCUSSED(@!)" "WIP(w!)" "PR(@)" "MERGED" "DEPLOYED" "DONE")))
+        (sequence "TODO" "DISCUSSED(@!)" "WIP(w!)" "PR(@)" "MERGED" "DEPLOYED" "DONE"))
+      goto-interface 'outline-path-completion
+      org-outline-path-complete-in-steps nil)
+
+(provide 'spm-org)
+;;; spm-org.el ends here
