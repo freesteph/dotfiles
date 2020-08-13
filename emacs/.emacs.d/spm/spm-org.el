bb230e8150fb73eeaa136da42e49a2b2a3fdd605
Stéphane Maniaci <stephane.maniaci@gmail.com>
2020-08-13 11:56:56 +0100

    emacs: more org tweaks

--- emacs/.emacs.d/spm/spm-org.el
+++ emacs/.emacs.d/spm/spm-org.el
@@ -3,6 +3,7 @@
 ;;; Commentary:
 ;;; Customisation for org.
 
+(require 'org-tempo)
 ;;; Code:
 (setf org-todo-keywords
       '((sequence "TODO" "DONE")
@@ -10,6 +11,13 @@
       goto-interface 'outline-path-completion
       org-outline-path-complete-in-steps nil)
 
+
+;; Example configuration
+(setq counsel-org-goto-display-style 'path)
+(setq counsel-org-goto-separator " ➜ ")
+(setq counsel-org-goto-face-style 'org)
+(define-key org-mode-map (kbd "C-c C-j") 'counsel-org-goto)
+(define-key org-mode-map (kbd "C-u C-c C-j") 'counsel-org-goto-all)
 
 (org-add-link-type "key" 'identity
                    (lambda (path desc backend)
@@ -19,7 +27,7 @@
                        (t path))))
 
 (setq org-todo-keyword-faces
-      '(("TODO" . org-warning) ("WIP" . "yellow")
+      '(("TODO" . org-warning)
         ("MERGED" . "blue")))
 
 (provide 'spm-org)
