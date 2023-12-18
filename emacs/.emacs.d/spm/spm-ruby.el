2bbb9acf09922ed2befefba8bc1de9457d79de00
Stéphane Maniaci <stephane.maniaci@gmail.com>
2023-12-18 16:39:28 +0100

    ruby: add a nasty way to lookup Rails/Rubocop docs
    This leaves room for improvement but it's a start.

--- emacs/.emacs.d/spm/spm-ruby.el
+++ emacs/.emacs.d/spm/spm-ruby.el
@@ -25,5 +25,29 @@
             (local-set-key (kbd "C-c C-f") 'spm/ruby/toggle-rspec-focus)))
 
 (setf ruby-insert-encoding-magic-comment nil)
+
+# Rubocop stuff
+(defun spm/rubocop/error-to-anchor (name)
+  "Get the last component of a slash-separated NAME and downcase it."
+  (downcase (mapconcat 'identity (split-string name "/"))))
+
+(defun spm/rubocop/get-next-error-id ()
+  "Find the next error on the line and return its id."
+  (save-excursion
+    (move-beginning-of-line nil)
+    (let* ((pos (flycheck-next-error-pos 1))
+           (err (get-char-property pos 'flycheck-error)))
+      (flycheck-error-id err))))
+
+(defun spm/rubocop/lookup-next-rubocop-error ()
+  "Look up and browse the documentation for the next error."
+  (interactive)
+  (let* ((base-url "https://docs.rubocop.org/rubocop-rails/cops_rails.html")
+         (err-id (spm/rubocop/get-next-error-id))
+         (anchor (spm/rubocop/error-to-anchor err-id)))
+    (browse-url (format "%s#%s" base-url anchor))))
+
+(define-key ruby-mode-map (kbd "C-c h") 'spm/rubocop/lookup-next-rubocop-error)
+
 (provide 'spm-ruby)
 ;;; spm-ruby.el ends here
